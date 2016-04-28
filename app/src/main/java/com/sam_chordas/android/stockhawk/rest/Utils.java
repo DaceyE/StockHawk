672ba848bdfc562981b8a330bb7fe8753a70abc2
dacey.ennis <barrett.dacey.ennis@gmail.com>
2016-04-28 00:56:18 -0400

    style: Standard white spacing applied to files.  No code change.
    - All the files were change to use the default whitespace.

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
@@ -7,6 +7,7 @@ import com.sam_chordas.android.stockhawk.data.StockHawkContract;
 import com.sam_chordas.android.stockhawk.data.StockHawkProvider;
 
 import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,82 +17,82 @@ import org.json.JSONObject;
  */
 public class Utils {
 
-  private static String LOG_TAG = Utils.class.getSimpleName();
+    private static String LOG_TAG = Utils.class.getSimpleName();
 
-  public static boolean showPercent = true;
+    public static boolean showPercent = true;
 
-  public static ArrayList quoteJsonToContentVals(String JSON){
-    ArrayList<ContentProviderOperation> batchOperations = new ArrayList<>();
-    JSONObject jsonObject = null;
-    JSONArray resultsArray = null;
-    try{
-      jsonObject = new JSONObject(JSON);
-      if (jsonObject != null && jsonObject.length() != 0){
-        jsonObject = jsonObject.getJSONObject("query");
-        int count = Integer.parseInt(jsonObject.getString("count"));
-        if (count == 1){
-          jsonObject = jsonObject.getJSONObject("results")
-              .getJSONObject("quote");
-          batchOperations.add(buildBatchOperation(jsonObject));
-        } else{
-          resultsArray = jsonObject.getJSONObject("results").getJSONArray("quote");
+    public static ArrayList quoteJsonToContentVals(String JSON) {
+        ArrayList<ContentProviderOperation> batchOperations = new ArrayList<>();
+        JSONObject jsonObject = null;
+        JSONArray resultsArray = null;
+        try {
+            jsonObject = new JSONObject(JSON);
+            if (jsonObject != null && jsonObject.length() != 0) {
+                jsonObject = jsonObject.getJSONObject("query");
+                int count = Integer.parseInt(jsonObject.getString("count"));
+                if (count == 1) {
+                    jsonObject = jsonObject.getJSONObject("results")
+                            .getJSONObject("quote");
+                    batchOperations.add(buildBatchOperation(jsonObject));
+                } else {
+                    resultsArray = jsonObject.getJSONObject("results").getJSONArray("quote");
 
-          if (resultsArray != null && resultsArray.length() != 0){
-            for (int i = 0; i < resultsArray.length(); i++){
-              jsonObject = resultsArray.getJSONObject(i);
-              batchOperations.add(buildBatchOperation(jsonObject));
+                    if (resultsArray != null && resultsArray.length() != 0) {
+                        for (int i = 0; i < resultsArray.length(); i++) {
+                            jsonObject = resultsArray.getJSONObject(i);
+                            batchOperations.add(buildBatchOperation(jsonObject));
+                        }
+                    }
+                }
             }
-          }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "String to JSON failed: " + e);
         }
-      }
-    } catch (JSONException e){
-      Log.e(LOG_TAG, "String to JSON failed: " + e);
+        return batchOperations;
     }
-    return batchOperations;
-  }
 
-  public static String truncateBidPrice(String bidPrice){
-    bidPrice = String.format("%.2f", Float.parseFloat(bidPrice));
-    return bidPrice;
-  }
-
-  public static String truncateChange(String change, boolean isPercentChange){
-    String weight = change.substring(0,1);
-    String ampersand = "";
-    if (isPercentChange){
-      ampersand = change.substring(change.length() - 1, change.length());
-      change = change.substring(0, change.length() - 1);
+    public static String truncateBidPrice(String bidPrice) {
+        bidPrice = String.format("%.2f", Float.parseFloat(bidPrice));
+        return bidPrice;
     }
-    change = change.substring(1, change.length());
-    double round = (double) Math.round(Double.parseDouble(change) * 100) / 100;
-    change = String.format("%.2f", round);
-    StringBuffer changeBuffer = new StringBuffer(change);
-    changeBuffer.insert(0, weight);
-    changeBuffer.append(ampersand);
-    change = changeBuffer.toString();
-    return change;
-  }
 
-  public static ContentProviderOperation buildBatchOperation(JSONObject jsonObject){
-    ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(
-        StockHawkProvider.Quotes.CONTENT_URI);
-    try {
-      String change = jsonObject.getString("Change");
-      builder.withValue(StockHawkContract.QoutesColumns.SYMBOL, jsonObject.getString("symbol"));
-      builder.withValue(StockHawkContract.QoutesColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
-      builder.withValue(StockHawkContract.QoutesColumns.PERCENT_CHANGE, truncateChange(
-          jsonObject.getString("ChangeinPercent"), true));
-      builder.withValue(StockHawkContract.QoutesColumns.CHANGE, truncateChange(change, false));
-      builder.withValue(StockHawkContract.QoutesColumns.ISCURRENT, 1);
-      if (change.charAt(0) == '-'){
-        builder.withValue(StockHawkContract.QoutesColumns.ISUP, 0);
-      }else{
-        builder.withValue(StockHawkContract.QoutesColumns.ISUP, 1);
-      }
-
-    } catch (JSONException e){
-      e.printStackTrace();
+    public static String truncateChange(String change, boolean isPercentChange) {
+        String weight = change.substring(0, 1);
+        String ampersand = "";
+        if (isPercentChange) {
+            ampersand = change.substring(change.length() - 1, change.length());
+            change = change.substring(0, change.length() - 1);
+        }
+        change = change.substring(1, change.length());
+        double round = (double) Math.round(Double.parseDouble(change) * 100) / 100;
+        change = String.format("%.2f", round);
+        StringBuffer changeBuffer = new StringBuffer(change);
+        changeBuffer.insert(0, weight);
+        changeBuffer.append(ampersand);
+        change = changeBuffer.toString();
+        return change;
     }
-    return builder.build();
-  }
+
+    public static ContentProviderOperation buildBatchOperation(JSONObject jsonObject) {
+        ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(
+                StockHawkProvider.Quotes.CONTENT_URI);
+        try {
+            String change = jsonObject.getString("Change");
+            builder.withValue(StockHawkContract.QoutesColumns.SYMBOL, jsonObject.getString("symbol"));
+            builder.withValue(StockHawkContract.QoutesColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
+            builder.withValue(StockHawkContract.QoutesColumns.PERCENT_CHANGE, truncateChange(
+                    jsonObject.getString("ChangeinPercent"), true));
+            builder.withValue(StockHawkContract.QoutesColumns.CHANGE, truncateChange(change, false));
+            builder.withValue(StockHawkContract.QoutesColumns.ISCURRENT, 1);
+            if (change.charAt(0) == '-') {
+                builder.withValue(StockHawkContract.QoutesColumns.ISUP, 0);
+            } else {
+                builder.withValue(StockHawkContract.QoutesColumns.ISUP, 1);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return builder.build();
+    }
 }
