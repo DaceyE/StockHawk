68070bd6c4b3287d304206c16c051bcc34653972
dacey.ennis <barrett.dacey.ennis@gmail.com>
2016-04-29 16:05:41 -0400

    fix: withSymbol now selects the correct table for historical data

--- app/src/main/java/com/sam_chordas/android/stockhawk/data/StockHawkProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/StockHawkProvider.java
@@ -65,7 +65,7 @@ public class StockHawkProvider {
                 pathSegment = 1
         )
         public static Uri withSymbol(String symbol) {
-            return buildUri(Path.QUOTES, symbol);
+            return buildUri(Path.HISTORICAL_DATA, symbol);
         }
     }
 
